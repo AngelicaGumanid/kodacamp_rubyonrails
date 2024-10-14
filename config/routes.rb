7aa08977d33aafd3f64cfc00d98a5210f505f363
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-14 12:05:46 +0800

    Add: Active records associations - created comment (add, edit and delete) function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # get 'posts/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "welcome#index"
-  resources :posts
+  resources :posts do
+    resources :comments, except: :show
+  end
 end
