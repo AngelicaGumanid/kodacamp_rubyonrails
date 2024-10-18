1825aba0cf93976db34f718babe2883243fb50c7
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-18 10:43:53 +0800

    add: project enhancment - comment count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_17_104639) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_18_023937) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_17_104639) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_count"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
